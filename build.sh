7c6f7c8e120a3c9eaf5d2ea8aa18e98553eba3fd
Andres <beco20@yahoo.com.ar>
2025-12-26 14:36:54 -0300

    config: CORS y ajustes para despliegue en Render

--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 # exit on error
 set -o errexit
 
+# Instalar librerías del sistema necesarias para GeoDjango
+# Usamos apt-get para instalar las dependencias de mapas en Render
+if [ "$RENDER" ]; then
+  apt-get update && apt-get install -y binutils libproj-dev gdal-bin
+fi
+
 # Instalar dependencias de Python
 pip install -r requirements.txt
 
-# Recolectar archivos estáticos
+# Recolectar estáticos y migrar
 python manage.py collectstatic --no-input
-
-# Ejecutar migraciones
 python manage.py migrate
